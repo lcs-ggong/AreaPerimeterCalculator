25a30d98f9f4c73f44e3082e65efcc98f00f664b
Grace Gong <ggong@lcs.on.ca>
2023-01-19 11:05:12 -0500

    Initial Commit

---
+++ AreaPerimeterCalculator/AreaPerimeterCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AreaPerimeterCalculatorApp.swift
+//  AreaPerimeterCalculator
+//
+//  Created by Grace Gong on 2023-01-19.
+//
+
+import SwiftUI
+
+@main
+struct AreaPerimeterCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
